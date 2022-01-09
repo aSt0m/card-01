8c350d22ed04782f0fdf858add8198faa3cecb8e
aSt0m <61126327+aSt0m@users.noreply.github.com>
2022-01-09 00:05:31 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+
+/* variables */
+/*  
+container = document.querySelector('.container');
+card = document.querySelector('.card');
+cardUp = document.querySelector('.card-up');
+cardDown = document.querySelector('.card-down');
+
+
+cardUp.addEventListener('mouseover', ()=>{
+
+    cardDown.style.height = '200px';
+    cardDown.style.transition = 'ease 1s';
+})
+cardUp.addEventListener('mouseout',()=>{
+    cardDown.style.height = '0';
+    cardDown.style.transition = '1s';
+
+})
+
+containerArray = Array.from( "container")
+console.log( containerArray );
+*/
