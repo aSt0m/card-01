59342c8c599585f619e49ff6c5611d67479c48f4
aSt0m <61126327+aSt0m@users.noreply.github.com>
2022-01-08 23:54:43 -0600

    Add files via upload
    Versión de YouTube para revisar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 
 /* variables */
-
+/*  
 container = document.querySelector('.container');
 card = document.querySelector('.card');
 cardUp = document.querySelector('.card-up');
@@ -11,12 +11,14 @@ cardDown = document.querySelector('.card-down');
 cardUp.addEventListener('mouseover', ()=>{
 
     cardDown.style.height = '200px';
-    cardDown.style.transition = '1s';
+    cardDown.style.transition = 'ease 1s';
 })
 cardUp.addEventListener('mouseout',()=>{
     cardDown.style.height = '0';
+    cardDown.style.transition = '1s';
 
 })
 
 containerArray = Array.from( "container")
 console.log( containerArray );
+*/
